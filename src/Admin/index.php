7c60d59dfc2311f3068f3552eff8e9ccfc3539c5
Venkata gnana vardhani Kanamarlapudi <vardhanikanamarlapudi@gmail.com>
2022-12-05 13:28:28 -0500

    Update index.php

--- src/Admin/index.php
+++ src/Admin/index.php
@@ -374,7 +374,7 @@
                                                     <td colspan="4">
                                                     <br><br><br><br>
                                                     <center>
-                                                    <img src="../img/notfound.svg" width="25%">
+                                                    <img src="../img/notfound.png" width="25%">
                                                     
                                                     <br>
                                                     <p class="heading-main12" style="margin-left: 45px;font-size:20px;color:rgb(49, 49, 49)">We  couldnt find anything related to your keywords !</p>
