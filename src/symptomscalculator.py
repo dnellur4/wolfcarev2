d8a99ce6bdaacee0fd9839ba11b0f5ca9da97cf1
dedeepya nelluru <dnellur@ncsu.edu>
2022-12-05 23:28:23 +0000

    Automated autoyapf fixes

--- src/symptomscalculator.py
+++ src/symptomscalculator.py
@@ -1,82 +1,77 @@
 class symptomsCalculator:
-    
     '''The given class calculates all the symptoms of the Diseases using the required constraints from the Medical Reports and outputs the relative result'''
 
     def acidBaseDisorder(self, minbicarbonate, maxbicarbonate):
-        
         '''The function calculates whether the patient has Acid Base Disorder utilizing its Medical Report's Constraints'''
-        
+
         # For Metabolic Acidosis
-        if (minbicarbonate < 18.0): 
+        if (minbicarbonate < 18.0):
             a = True
-        else: 
+        else:
             a = False
-        
+
         #For Metabolic Alkalosis
-        if (maxbicarbonate > 30.0): 
+        if (maxbicarbonate > 30.0):
             b = True
-        else: 
+        else:
             b = False
-        
+
         return a, b
-    
+
     def hyperCholesterolemia(self, maxcholestrol):
-        
         '''The function calculates whether the patient has Hyper Cholesterolemia utilizing its Medical Report's Constraints'''
-        
-        if (maxcholestrol > 200): 
+
+        if (maxcholestrol > 200):
             return True
         else:
             return False
-    
+
     def hypertension(self, sbp, dbp):
-        
         '''The function calculates whether the patient has Hypertension utilizing its Medical Report's Constraints'''
         if (sbp > 140) and (dbp > 90):
-            
+
             return True
-        else: 
+        else:
             return False
-    
+
     def kidneyInjury(self, maxCreatinine, prevMaxCreatinine):
-        
         '''The function calculates whether the patient has Kidney Injury utilizing its Medical Report's Constraints'''
-        
+
         # For Acute
-        if (((maxCreatinine) > (1.5 * prevMaxCreatinine)) or ((maxCreatinine > 1.2) and (prevMaxCreatinine <= 1.2))):
+        if (((maxCreatinine) > (1.5 * prevMaxCreatinine))
+                or ((maxCreatinine > 1.2) and (prevMaxCreatinine <= 1.2))):
             a = True
-        else: 
+        else:
             a = False
-        
+
         #For Chronic
-        if ((maxCreatinine > 1.2) and (prevMaxCreatinine > 1.2)): 
+        if ((maxCreatinine > 1.2) and (prevMaxCreatinine > 1.2)):
             b = True
-        else: 
+        else:
             b = False
-        
+
         #For Acute on Chronic
-        if ((maxCreatinine > 1.2) and (prevMaxCreatinine > 1.2) and ((maxCreatinine) > (1.5 * prevMaxCreatinine))): 
+        if ((maxCreatinine > 1.2) and (prevMaxCreatinine > 1.2)
+                and ((maxCreatinine) > (1.5 * prevMaxCreatinine))):
             c = True
         else:
             c = False
-        
+
         #For Not otherwise specified
         if (maxCreatinine > 1.2):
             d = True
         else:
             d = False
-            
+
         return a, b, c, d
-        
+
     def obesity(self, height, weight):
-        
         '''The function calculates whether the patient has Obesity utilizing its Medical Report's Constraints'''
-        
+
         if (height > 0) and (weight > 0):
-            bmi = (weight)/((height/100)*(height/100))
-        
+            bmi = (weight) / ((height / 100) * (height / 100))
+
         if bmi > 30:
             return True
         else:
             return False
-        
--- tests/test_symptomscalculator.py
+++ tests/test_symptomscalculator.py
@@ -1,6 +1,7 @@
 import sys
+
 sys.path.append("../src/")
-print("xcvbnm,",sys.path.append("../src/"))
+print("xcvbnm,", sys.path.append("../src/"))
 
 import pytest
 import unittest
@@ -9,44 +10,42 @@ from symptomscalculator import symptomsCalculator
 
 a = symptomsCalculator()
 
+
 class testReturnValues(unittest.TestCase):
     '''The given class tests all the symptoms of the Diseases and outputs the relative result'''
-    
+
     def test_acidBaseDisorder(self):
-        
         '''The function determines whether the patient has Acid Base Disorder utilizing its Medical Report's Constraints'''
-        
-        temp1, temp2 = a.acidBaseDisorder(15, 31) 
+
+        temp1, temp2 = a.acidBaseDisorder(15, 31)
         self.assertEqual(temp1, True) and self.assertEqual(temp2, False)
-    
+
     def test_hyperCholesterolemia(self):
-        
         '''The function determines whether the patient has Hyper Cholesterolemia utilizing its Medical Report's Constraints'''
-        
-        temp = a.hyperCholesterolemia(160) 
+
+        temp = a.hyperCholesterolemia(160)
         self.assertEqual(temp, False)
-    
+
     def test_hypertension(self):
-        
         '''The function determines whether the patient has Hypertension utilizing its Medical Report's Constraints'''
-        
-        temp = a.hypertension(145, 95) 
-        self.assertEqual(temp, True) 
-    
+
+        temp = a.hypertension(145, 95)
+        self.assertEqual(temp, True)
+
     def test_kidneyInjury(self):
-        
         '''The function determines whether the patient has Kidney Injury utilizing its Medical Report's Constraints'''
-        
-        temp1, temp2, temp3, temp4 = a.kidneyInjury(1.5, 1.1) # pragma: no cover
-        if ((temp1 == True) and (temp2 == False) and (temp3 == False) and (temp4 == True)): 
+
+        temp1, temp2, temp3, temp4 = a.kidneyInjury(1.5,
+                                                    1.1)  # pragma: no cover
+        if ((temp1 == True) and (temp2 == False) and (temp3 == False)
+                and (temp4 == True)):
             temp = True
-        else: # pragma: no cover
+        else:  # pragma: no cover
             temp = False
-        self.assertEqual(temp, True) 
-    
+        self.assertEqual(temp, True)
+
     def test_obesity(self):
-        
         '''The function determines whether the patient has Obesity utilizing its Medical Report's Constraints'''
-        
-        temp = a.obesity(180, 72) 
-        self.assertEqual(temp, False) 
+
+        temp = a.obesity(180, 72)
+        self.assertEqual(temp, False)
