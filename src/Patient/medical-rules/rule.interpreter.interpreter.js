71a168d749d82f394e7ec1d46cf59903d7ebe904
dnellur4 <dnellur4@users.noreply.github.com>
2022-12-06 00:42:29 +0000

    Apply prettier changes

--- src/Patient/medical-rules/rule.interpreter.interpreter.js
+++ src/Patient/medical-rules/rule.interpreter.interpreter.js
@@ -3,8 +3,7 @@
  */
 
 // processedResult - contains the evaluated(true/false) rules for previously defined comorbidities.
-function parseRule(rule, variables, processedResult)
-{        
+function parseRule(rule, variables, processedResult) {
     var tokens = lexer(rule);
     //console.log(tokens);
     var parseTree = parse(tokens);
@@ -13,28 +12,24 @@ function parseRule(rule, variables, processedResult)
     return output;
 }
 
-var processRules = function(ruleJSON, variables){
+var processRules = function (ruleJSON, variables) {
     var result = [];
-    for(var i = 0; i < ruleJSON.length; i++) {
-        var obj = ruleJSON[i];     
+    for (var i = 0; i < ruleJSON.length; i++) {
+        var obj = ruleJSON[i];
         var key = Object.keys(obj)[0];
-        result[key] = {}
+        result[key] = {};
         var value = obj[key];
-        if (typeof(value) == 'object')
-        {
-            for (var j = 0; j < value.length; j++)
-            {
+        if (typeof value == 'object') {
+            for (var j = 0; j < value.length; j++) {
                 var valObj = value[j];
                 var valKey = Object.keys(valObj)[0];
-                var valRule = valObj[valKey]; 
+                var valRule = valObj[valKey];
                 var comorbidityPresent = parseRule(valRule, variables, result);
                 result[key][valKey] = comorbidityPresent;
             }
-        }
-        else
-        {                     
+        } else {
             result[key] = parseRule(value, variables, result);
-        }    
+        }
     }
-    return result;    
-}
+    return result;
+};
--- src/Patient/medical-rules/ruleserver.js
+++ src/Patient/medical-rules/ruleserver.js
@@ -1,5 +1,4 @@
-
-var express = require("express");
+var express = require('express');
 var app = express();
-app.use(express.static("./")).listen(3000);
+app.use(express.static('./')).listen(3000);
 module.exports = app;
